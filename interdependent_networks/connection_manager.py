4e4b28e6e789b1c977fa120defbfc60492cd298d
Felipe Quintanilla <fquintan.m@gmail.com>
2020-09-09 13:02:45 -0300

    espera response en set_job_done

--- interdependent_networks/connection_manager.py
+++ interdependent_networks/connection_manager.py
@@ -27,6 +27,9 @@ class ConnectionManager:
         self.connection = http.client.HTTPConnection(self.server_name)
         uri = "/job_done/" + str(job_id)
         self.connection.request("GET", uri)
+        json_data = self.connection.getresponse()
+        return get_lines_from_json(json_data)
+
 
     def get_jobs_from_server(self, n_workers):
         self.connection = http.client.HTTPConnection(self.server_name)
