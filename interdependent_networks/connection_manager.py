d4433a19b25e8afb141d9f62af67a7eb4fec85c0
Felipe Quintanilla <fquintan.m@gmail.com>
2020-09-09 07:56:20 -0300

    reconnect en cada operacion en ConnectionManager

--- interdependent_networks/connection_manager.py
+++ interdependent_networks/connection_manager.py
@@ -16,18 +16,20 @@ class ConnectionManager:
     connection = None
     machine_name = None
 
-    def __init__(self, server_name):
+    def __init__(self, server_name, machine_name=""):
         self.server_name = server_name
-        self.connection = http.client.HTTPConnection(server_name)
+        self.machine_name = machine_name
 
     def set_machine_name(self, machine_name):
         self.machine_name = machine_name
 
     def set_job_done(self, job_id):
+        self.connection = http.client.HTTPConnection(self.server_name)
         uri = "/job_done/" + str(job_id)
         self.connection.request("GET", uri)
 
     def get_jobs_from_server(self, n_workers):
+        self.connection = http.client.HTTPConnection(self.server_name)
         uri = "/get_jobs/" + str(n_workers)
         if self.machine_name:
             uri += "?machine="+str(self.machine_name)
