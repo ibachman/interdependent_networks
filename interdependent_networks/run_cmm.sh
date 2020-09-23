5699ef8af39d955b67b7dc76a7d5b99e9e1b4fc1
Ivana Bachmann <ivana.bachmann@gmail.com>
2020-09-23 17:14:41 -0300

    added new runnable files

---
+++ interdependent_networks/run_cmm.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+mkdir -p test_results/degree
+mkdir -p test_results/distance
+mkdir -p test_results/random
+mkdir -p test_results/simple_graphs
+
+pip3 install -r requirements.txt
+
+python3 job_manager.py -f all_experiments.txt -w 3
