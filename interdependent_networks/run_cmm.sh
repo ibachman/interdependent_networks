1101ab33e2e38bd3a962bbedbd84cf29ef64da7c
Ivana Bachmann <ivana.bachmann@gmail.com>
2020-09-30 16:47:28 -0300

    new run_cmm.sh that connects to a server

--- interdependent_networks/run_cmm.sh
+++ interdependent_networks/run_cmm.sh
@@ -1,4 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+
+#SBATCH -J niclabs
+#SBATCH -c 44
+#SBATCH -p general
+#SBATCH --output=exp_server_%A.out
+#SBATCH --mail-user=ivana@niclabs.cl (--mail-user=ivana@niclabs.cl)
+#SBATCH --mail-type=ALL
+
+ml Python/3.8.2
+
 mkdir -p test_results/degree
 mkdir -p test_results/distance
 mkdir -p test_results/random
@@ -6,4 +16,4 @@ mkdir -p test_results/simple_graphs
 
 pip3 install -r requirements.txt
 
-python3 job_manager.py -f all_experiments.txt -w 88
+srun python3 -u job_manager.py -g jobs-api.felipequintanilla.cl -m cmm -w 44
