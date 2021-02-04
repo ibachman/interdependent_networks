824caa67790dd2c6582d7d2c8feee5befcd47352
Ivana Bachmann <ivana.bachmann@gmail.com>
2021-02-04 16:39:43 -0300

    Concurrent job handler modified to handle seismic simulations

--- interdependent_networks/concurrent_run.py
+++ interdependent_networks/concurrent_run.py
@@ -1,5 +1,5 @@
 from runnables import run_test, add_edges
-# from interdependent_networks.runnables import run_test, add_edges
+#from interdependent_networks.runnables import run_test, add_edges
 #
 import multiprocessing
 import argparse
@@ -27,7 +27,9 @@ def worker_run(worker_queue):
                      task['n_logic_suppliers'], task['version'], task['n_logic'], task['n_phys'],
                      task['iter'], task['READ_flag'], task['attack_type'], task['model'], task['logic'],
                      task['physical'], task['phys_iteration'], task['strategy'],
-                     localized_attack_data=task['localized_attacks'], process_name=process_name)
+                     localized_attack_data=task['localized_attacks'],
+                     process_name=process_name,
+                     seismic_data=task['seismic_data'])
         # avisar que job_done (task["job_id"])
         if task["job_id"] > -1:
             task["server_connection"].set_job_done(task["job_id"])
@@ -94,9 +96,11 @@ def parse_task_args(line):
     task['localized_attacks_radius'] = args.localizedattacksradius
     task['localized_attacks_center'] = args.localizedattackscenter
     task['localized_attacks_file'] = args.localizedattacksfile
+
+    task['seismic_data_file'] = args.seismicdatafile
+
     localized_attack_data = {}
     if task['localized_attacks_center']:
-
         x_center = float(task['localized_attacks'][0])
         y_center = float(task['localized_attacks'][1])
         localized_attack_data["x_center"] = x_center
@@ -106,8 +110,19 @@ def parse_task_args(line):
     if task['localized_attacks_file']:
         localized_attack_data["file"] = task['localized_attacks_file']
     task['localized_attacks'] = localized_attack_data
+
+    seismic_data = {}
+    if task['seismic_data_file']:
+        print(task['seismic_data_file'])
+        seismic_data["file"] = task['seismic_data_file']
+    task['seismic_data'] = seismic_data
+
     if len(localized_attack_data) > 0:
         task['attack_type'] = ['localized']
+
+    if len(seismic_data) > 0:
+        task['attack_type'] = ['seismic']
+
 
     return task
 
@@ -184,6 +199,7 @@ parser.add_argument('-lar', '--localizedattacksradius',  nargs='*', help='Test l
 parser.add_argument('-lac', '--localizedattackscenter',  nargs=2, help='Test localized attacks. Receives [x_center, y_center]')
 parser.add_argument('-laf', '--localizedattacksfile',  nargs=1, help='Test localized attacks. Receives filename]')
 
+parser.add_argument('-sf', '--seismicdatafile',type=str, help='Test seismic attacks. Receives filename]')
 
 if __name__ == "__main__":
     line = " ".join(sys.argv[1::])
